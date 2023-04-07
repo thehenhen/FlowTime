392219998c19a9c882abfe90eb4287eff3f6234d
Henry Zhang <sirhenryzhang@gmail.com>
2023-04-07 13:51:20 -0400

    testing local storage

--- js/sketch.js
+++ js/sketch.js
@@ -4,11 +4,26 @@ function setup(){
     background(255);
     textAlign(CENTER,CENTER);
     text("This text was generated using p5.js",width/2,height/2); 
+    score=parseInt(localStorage.getItem('score'))||0;
+
 }
 let x=0;
+let score=0;
 function draw(){
     background(0);
     fill(100);
     ellipse(x,height/2,10,10);
+    fill(255);
+    text(score,100,100);
     x++; 
+}
+
+function mousePressed(){
+    score++;
+    localStorage.setItem('score',score);
+}
+
+function keyPressed(){
+    score=0;
+    localStorage.setItem('score',score);
 }
